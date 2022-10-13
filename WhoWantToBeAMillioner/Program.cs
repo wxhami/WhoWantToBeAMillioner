d3b2fafd0790bdf13f77b40094aa99b528637df6
wxhami <d18102002@mail.ru>
2022-10-13 20:53:02 +0300

    add implement GetQuestion

--- WhoWantToBeAMillioner/Program.cs
+++ WhoWantToBeAMillioner/Program.cs
@@ -1,2 +1,27 @@
-﻿using WhoWantToBeAMillioner;
+﻿using System.Net.Mime;
+using WhoWantToBeAMillioner;
 
+QuestionProvider questionProvider = new QuestionProvider();
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Question question = questionProvider.GetQuestion(i);
+    Console.WriteLine(question.Text);
+    Console.WriteLine("Choose answer");
+    for (int j = 1; j <= 4; j++)
+    {
+        
+        Console.WriteLine(question.Answers[j]);
+    }
+
+    int playerChoice = Convert.ToInt32(Console.ReadLine());
+    if (question.Answers[playerChoice].IsRight == false)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("You so smart ;)");
+    }
+}
--- WhoWantToBeAMillioner/QuestionProvider.cs
+++ WhoWantToBeAMillioner/QuestionProvider.cs
@@ -2,11 +2,23 @@
 
 public class QuestionProvider
 {
-    public Question[] Questions;
+    public List<Question> Questions;
 
     public Question GetQuestion(int level)
     {
+        var questions = new List<Question>();
         
+        foreach (Question question in Questions)
+        {
+            if (level == question.Level)
+            {
+                questions.Add(question);
+            }
+        }
+
+        var random = new Random();
+
+        return questions[random.Next(0, questions.Count)];
     }
 }
 
